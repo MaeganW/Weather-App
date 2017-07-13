8831b90eb25c801c14ca3c69c4e333838e632284
Maegan Womble <maeganwomble@gmail.com>
2017-07-12 21:27:34 -0500

    DOM shows city, state, and temp

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 var latitude;
 var longitude;
+var state;
+var city;
+var temp;
 
 function getLocation() {
     if (navigator.geolocation) {
@@ -13,14 +16,27 @@ function showPosition(position) {
     latitude = position.coords.latitude;
     longitude = position.coords.longitude;
     console.log("latitude: " + latitude + " longitude: " + longitude);
-    
-    $.ajax({
-        url: "http://api.wunderground.com/api/657659e007fc1bed/conditions/q/CA/San_Francisco.json",
-        success: function (obj) {
-            console.log(obj.current_observation.temp_f);
 
+    $.ajax({
+        url: "http://api.wunderground.com/api/657659e007fc1bed/geolookup/q/" + latitude + "," + longitude + ".json",
+        success: function (obj) {
+            $(".location-display").html(obj.location.city + " , " + obj.location.state);
+
+            state = obj.location.state;
+            var unformattedCity = obj.location.city;
+            city = unformattedCity.replace("/ /g,_");
         }
     });
 }
 
-
+function getWeather() {
+    $.ajax({
+        url: "http://api.wunderground.com/api/657659e007fc1bed/conditions/q/" + state + "/" + city + ".json", success: function(cityObj){
+            console.log(cityObj);
+            console.log(cityObj.current_observation.temp_f);
+            
+            temp = cityObj.current_observation.temp_f;
+            $(".temp-display").html(temp + " F");
+        }
+    });
+}
